73fa602efaba4548a8525395bcc7373b556ced38
atombor <atombor@gmail.com>
2016-05-03 17:47:02 +0200

    elso kommit

---
+++ src/erdo/Erdo.java
@@ -0,0 +1,16 @@
+package erdo;
+
+/**
+ * Created by doki on 2016.05.03..
+ */
+public class Erdo {
+
+
+    public static void main(String args[]) {
+
+        System.out.println("Erdő erdő erdőőő!");
+
+    }
+
+
+}
